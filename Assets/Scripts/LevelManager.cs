0225fe410cc5435356e98b4a4bfee3d848ed4ebd
Ryan Morgan <Morganglassworks@gmail.com>
2017-07-19 13:12:50 -0700

    Sound to splash screen update menu
    added 3 seconds then auto-moves to main screen while playing sound, improved menu system of main window

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+
+	void Start (){
+		Invoke ("LoadNextLevel", autoLoadNextLevelAfter); 
+	}
+
+	public void LoadLevel(string name){
+		Debug.Log ("Level Load Requested for: " + name);
+		Application.LoadLevel(name); 
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Player would like to quit");
+		Application.Quit();
+	}
+	
+	public void LoadNextLevel() {
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+
+
+}
